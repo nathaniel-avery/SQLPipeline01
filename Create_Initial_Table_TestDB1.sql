483db4d7dd927575715f627a3fde310b69812ec6
nathaniel-avery <navery@actionet.com>
2018-04-23 17:30:01 -0400

    broke out SQL scripts

---
+++ Create_Initial_Table_TestDB1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Customers]
+( 
+    fname [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL, 
+    lname [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,  
+    state [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL) ON [PRIMARY]
+    );
---
+++ Create_Initial_Values_TestDB1.sql
@@ -0,0 +1,4 @@
+-- Populates the DB with some simple data
+INSERT INTO Customers values ('Bruce', 'Wayne','Gotham') 
+INSERT INTO Customers values ('Clark', 'Kent','Metropolis')
+INSERT INTO Customers values ('Barry', 'Allen','Central City');
